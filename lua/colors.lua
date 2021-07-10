59ffb68a229f7bbe895a593c7809a29930ef9579
ashincoder <ashinant15@gmail.com>
2021-07-10 19:00:13 +0530

    adding colors

---
+++ lua/colors.lua
@@ -0,0 +1,30 @@
+local g = vim.g
+-- Neon Colorscheme
+
+-- There are 4 styles for neon colorscheme
+-- 'default' 'doom' 'dark' 'light'
+-- g.neon_style = "doom"
+-- g.neon_italic_keyword = true
+-- g.neon_italic_function = true
+
+-------------------------------------------------------------
+-- Tokyonight Colorscheme
+
+-- [[ There are 3 styles for tokyonight colorscheme
+-- 'storm' 'night' 'day' ]]
+
+g.tokyonight_style = "night"
+g.tokyonight_italic_functions = true
+g.tokyonight_italic_comments = true
+g.tokyonight_sidebars = {"qf", "vista_kind", "terminal", "packer"}
+
+-------------------------------------------------------------
+-- Onedark Colorscheme
+
+-- There are 6 styles for onedark colorscheme
+-- 'dark' 'darker' 'cool' 'deep' 'warm' 'warmer'
+
+-- g.onedark_style = 'deep'
+
+g.colors_name = Kv.colorscheme
+
