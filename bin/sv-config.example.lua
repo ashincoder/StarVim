a11bba0fbb658dda634cbf2b077ed628ecac740e
ashincoder <ashinant15@gmail.com>
2021-07-13 11:13:26 +0530

    Fixed error in installer updated example config

--- bin/sv-config.example.lua
+++ bin/sv-config.example.lua
@@ -16,19 +16,25 @@ Sv.colorscheme = "tokyonight"
 Sv.autosave = false
 Sv.format_on_save = true
 
+-- TODO will add more plugins
+-- Disable or Enable Plugins
+Sv.plugin = {
+	dashboard_disable = 0,
+}
+
 -- TreeSitter parsers config
 -- Sv.treesitter.ensure_installed = "all"
 Sv.treesitter.ignore_install = { "haskell" }
 Sv.treesitter.highlight.enabled = true
 
 -- lua
-Sv.lua.lang.formatter.exe = "stylua"
+-- Sv.lua.lang.formatter.exe = "stylua"
 
 -- python
 -- Sv.lang.python.linter = 'flake8'
-Sv.lang.python.isort = true
-Sv.lang.python.diagnostics.virtual_text = true
-Sv.lang.python.analysis.use_library_code_types = true
+-- Sv.lang.python.isort = true
+-- Sv.lang.python.diagnostics.virtual_text = true
+-- Sv.lang.python.analysis.use_library_code_types = true
 -- to change default formatter from yapf to black
 -- Sv.lang.python.formatter.exe = "black"
 -- Sv.lang.python.formatter.args = {"-"}
@@ -38,13 +44,13 @@ Sv.lang.python.analysis.use_library_code_types = true
 -- Sv.lang.go.formatter.exe = "goimports"
 
 -- javascript
-Sv.lang.tsserver.linter = nil
+-- Sv.lang.tsserver.linter = nil
 
 -- rust
-Sv.lang.rust.formatter = {
-	exe = "rustfmt",
-	args = { "--emit=stdout" },
-}
+-- Sv.lang.rust.formatter = {
+-- 	exe = "rustfmt",
+-- 	args = { "--emit=stdout" },
+-- }
 
 -- Additional Plugins
 -- Sv.user_plugins = {
