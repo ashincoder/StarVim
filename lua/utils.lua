8405d4e4888b5a8d0bd22ef05fd00f1faa471370
ashincoder <ashinant15@gmail.com>
2021-07-14 13:15:35 +0530

    Reload

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,85 +1,86 @@
 local M = {}
 
 function M.reload_config()
-  vim.cmd "source ~/.config/nvim/lua/config.lua"
-  vim.cmd "source ~/.config/nvim/sv-config.lua"
-  vim.cmd "source ~/.config/nvim/lua/plugins.lua"
-  vim.cmd ":PackerInstall"
-  vim.cmd ":PackerCompile"
+	vim.cmd("source ~/.config/nvim/lua/config.lua")
+	vim.cmd("source ~/.config/nvim/sv-config.lua")
+	vim.cmd("source ~/.config/nvim/lua/plugins.lua")
+	vim.cmd("source ~/.config/nvim/lua/core/formatter.lua")
+	vim.cmd(":PackerInstall")
+	vim.cmd(":PackerCompile")
 end
 
 -- blankline config
 M.blankline = function()
-    vim.g.indentLine_enabled = 1
-    vim.g.indent_blankline_char = "▏"
+	vim.g.indentLine_enabled = 1
+	vim.g.indent_blankline_char = "▏"
 
-    vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
-    vim.g.indent_blankline_buftype_exclude = {"terminal"}
+	vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
+	vim.g.indent_blankline_buftype_exclude = { "terminal" }
 
-    vim.g.indent_blankline_show_trailing_blankline_indent = false
-    vim.g.indent_blankline_show_first_indent_level = false
+	vim.g.indent_blankline_show_trailing_blankline_indent = false
+	vim.g.indent_blankline_show_first_indent_level = false
 end
 
 function M.define_augroups(definitions) -- {{{1
-  -- Create autocommand groups based on the passed definitions
-  --
-  -- The key will be the name of the group, and each definition
-  -- within the group should have:
-  --    1. Trigger
-  --    2. Pattern
-  --    3. Text
-  -- just like how they would normally be defined from Vim itself
-  for group_name, definition in pairs(definitions) do
-    vim.cmd("augroup " .. group_name)
-    vim.cmd "autocmd!"
+	-- Create autocommand groups based on the passed definitions
+	--
+	-- The key will be the name of the group, and each definition
+	-- within the group should have:
+	--    1. Trigger
+	--    2. Pattern
+	--    3. Text
+	-- just like how they would normally be defined from Vim itself
+	for group_name, definition in pairs(definitions) do
+		vim.cmd("augroup " .. group_name)
+		vim.cmd("autocmd!")
 
-    for _, def in pairs(definition) do
-      local command = table.concat(vim.tbl_flatten { "autocmd", def }, " ")
-      vim.cmd(command)
-    end
+		for _, def in pairs(definition) do
+			local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
+			vim.cmd(command)
+		end
 
-    vim.cmd "augroup END"
-  end
+		vim.cmd("augroup END")
+	end
 end
 
-M.define_augroups {
+M.define_augroups({
 
-  _user_autocommands = Sv.user_autocommands,
-  _general_settings = {
-    {
-      "TextYankPost",
-      "*",
-      "lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})",
-    },
-    {
-      "BufWinEnter",
-      "*",
-      "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
-    },
-    {
-      "BufRead",
-      "*",
-      "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
-    },
-    {
-      "BufNewFile",
-      "*",
-      "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
-    },
-    { "BufWritePost", "lv-config.lua", "lua require('lv-utils').reload_lv_config()" },
-    -- { "VimLeavePre", "*", "set title set titleold=" },
-  }
-}
+	_user_autocommands = Sv.user_autocommands,
+	_general_settings = {
+		{
+			"TextYankPost",
+			"*",
+			"lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})",
+		},
+		{
+			"BufWinEnter",
+			"*",
+			"setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+		},
+		{
+			"BufRead",
+			"*",
+			"setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+		},
+		{
+			"BufNewFile",
+			"*",
+			"setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+		},
+		{ "BufWritePost", "lv-config.lua", "lua require('lv-utils').reload_lv_config()" },
+		-- { "VimLeavePre", "*", "set title set titleold=" },
+	},
+})
 -- hide line numbers , statusline in specific buffers!
 M.hideStuff = function()
-    vim.api.nvim_exec(
-        [[
+	vim.api.nvim_exec(
+		[[
    au BufEnter term://* setlocal nonumber
    au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif
    au BufEnter term://* set laststatus=0 
 ]],
-        false
-    )
+		false
+	)
 end
 
 return M
