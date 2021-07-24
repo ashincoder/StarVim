e1e0d1de3e3e55d8d4331e612aa89d2a9aaa39d0
ashincoder <83629316+ashincoder@users.noreply.github.com>
2021-07-24 11:44:31 +0530

    Changed Configs

--- bin/starrc.example.lua
+++ bin/starrc.example.lua
@@ -10,23 +10,19 @@ local star = {
 	},
 	star = {
 		-- 'orgmode',    -- Life Organization Tool
-		-- 'runner',   -- Open a REPL for the current language or run the current file
-		-- 'compiler', -- Compile (and run) your code with just pressing three keys!
 	},
 	editor = {
 		"lsp", -- Language Server Protocols
+		"completion", -- The ultimate completion
 		"nvim-tree", -- Tree explorer
 		"symbols", -- LSP symbols and tags
 		"gitsigns", -- Git signs
 		"telescope", -- Highly extendable fuzzy finder over lists
 		"formatter", -- File formatting
 		"autopairs", -- Autopairs
-		"commentary", -- Comments plugin
+		"commentary", -- Commentary plugin
 		-- "terminal", -- Terminal for Neovim (NOTE: needed for runner and compiler)
-		-- 'auto-session',    -- A small automated session manager for Neovim
 		-- 'minimap',      -- Code minimap, requires github.com/wfxr/code-minimap
-		-- 'restclient',   -- A fast Neovim http client
-		-- 'editorconfig', -- EditorConfig support for Neovim
 	},
 	utilities = {
 		"colorizer", -- Fastets colorizer for Neovim
