65a54968082bd396b9b1bd59689932598b431587
ashincoder <83629316+ashincoder@users.noreply.github.com>
2021-07-25 18:52:39 +0530

    Changes made

--- lua/core/keybindings/init.lua
+++ lua/core/keybindings/init.lua
@@ -22,10 +22,10 @@ utils.map("v", "x", [=[ "_x ]=], opt)
 utils.map("n", "<c-z>", "<Nop>", opts)
 
 -- Copy whole file content with Ctrl-a
-utils.map("n", "<C-a>", [[ <Cmd> %y+<CR>]], opts)
+utils.map("n", "<C-a>", "<Cmd>%y+<CR>", opts)
 
 -- Save with Ctrl-s
-utils.map("n", "<C-s>", ":w <CR>", opts)
+utils.map("n", "<C-s>", "<cmd>w <CR>", opts)
 
 -- Esc with jh
 utils.map("i", "jk", "<esc>", {})
@@ -36,21 +36,21 @@ utils.map("v", "<", "<gv", opts)
 utils.map("v", ">", ">gv", opts)
 
 -- use ESC to turn off search highlighting
-utils.map("n", "<Esc>", ":noh<CR>", opts)
+utils.map("n", "<Esc>", "<cmd>noh<CR>", opts)
 
 -- use ESC to turn off search highlighting
 utils.map("t", "<Esc>", "<C-\\><C-n>", opts)
 
 -- Move selected line / block of text in visual mode
-utils.map("x", "K", ":move '<-2<CR>gv-gv", opts)
-utils.map("x", "J", ":move '>+1<CR>gv-gv", opts)
+utils.map("x", "K", "<cmd>move '<-2<CR>gv-gv", opts)
+utils.map("x", "J", "<cmd>move '>+1<CR>gv-gv", opts)
 
 -- Don't copy the replaced text after pasting in visual mode
 utils.map("v", "p", '"_dP', opts)
 
 -- Commentary
-utils.map("n", "<leader>/", ":CommentToggle<CR>", opts)
-utils.map("v", "<leader>/", ":CommentToggle<CR>", opts)
+utils.map("n", "<leader>/", "<cmd>CommentToggle<CR>", opts)
+utils.map("v", "<leader>/", "<cmd>CommentToggle<CR>", opts)
 
 -- better window movement
 utils.map("n", "<C-h>", "<C-w>h", opts)
@@ -65,69 +65,71 @@ utils.map("n", "<leader>wv", "<C-W>v", opts) -- Vertical Split
 utils.map("n", "<leader>wl", "<C-W>5>", opts) -- Expand Split Right
 utils.map("n", "<leader>wh", "<C-W>5<", opts) -- Expand Split left
 utils.map("n", "<leader>wb", "<C-W>=", opts) --  Balance Splits
-utils.map("n", "<leader>wj", ":resize -5<CR>", opts) -- Expand Split above
-utils.map("n", "<leader>wk", ":resize +5<CR>", opts) -- Expand Split below
+utils.map("n", "<leader>wj", "<cmd>resize -5<CR>", opts) -- Expand Split above
+utils.map("n", "<leader>wk", "<cmd>resize +5<CR>", opts) -- Expand Split below
 
 -- Buffers or Tabs
-utils.map("n", "<leader>bn", ":enew<CR>", opts) -- New tab
-utils.map("n", "<leader>bd", ":bd!<CR>", opts) -- Close tab
-utils.map("n", "<TAB>", ":BufferLineCycleNext<CR>", opts) -- Next Tab
-utils.map("n", "<S-TAB>", ":BufferLineCyclePrev<CR>", opts) -- Prev Tab
-utils.map("n", "<leader>bp", ":BufferLinePick<CR>", opts) -- Pick a buffer or tab
+utils.map("n", "<leader>bn", "<cmd>enew<CR>", opts) -- New tab
+utils.map("n", "<leader>bd", "<cmd>bd!<CR>", opts) -- Close tab
+utils.map("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>", opts) -- Next Tab
+utils.map("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>", opts) -- Prev Tab
+utils.map("n", "<leader>bp", "<cmd>BufferLinePick<CR>", opts) -- Pick a buffer or tab
 
 -- Git
-utils.map("n", "<leader>gg", ":LazyGit<CR>", opts) -- Open LazyGit
-utils.map("n", "<leader>gc", ":Telescope git_commits<CR>", opts) -- Commits List
-utils.map("n", "<leader>gC", ":Telescope git_bcommits<CR>", opts) -- Commits List for current file
-utils.map("n", "<leader>gb", ":Telescope git_branches<CR>", opts) -- Branches List
-utils.map("n", "<leader>gt", ":Telescope git_status<CR>", opts) -- Git status
-utils.map("n", "<leader>gj", ":lua require 'gitsigns'.next_hunk()<CR>", opts) -- Next Hunk
-utils.map("n", "<leader>gk", ":lua require 'gitsigns'.prev_hunk()<CR>", opts) -- Prev Hunk
-utils.map("n", "<leader>gl", ":lua require 'gitsigns'.blame_line()<CR>", opts) -- Blame
-utils.map("n", "<leader>gp", ":lua require 'gitsigns'.preview_hunk()<CR>", opts) -- Preview Hunk
-utils.map("n", "<leader>gr", ":lua require 'gitsigns'.reset_hunk()<CR>", opts) -- Reset Hunk
-utils.map("n", "<leader>gR", ":lua require 'gitsigns'.reset_buffer()<CR>", opts) -- Reset Buffer
-utils.map("n", "<leader>gs", ":lua require 'gitsigns'.stage_hunk()<CR>", opts) -- Stage Hunk
-utils.map("n", "<leader>gu", ":lua require 'gitsigns'.undo_stage_hunk()<CR>", opts) -- Unstage Hunk
+utils.map("n", "<leader>gg", "<cmd>LazyGit<CR>", opts) -- Open LazyGit
+utils.map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", opts) -- Commits List
+utils.map("n", "<leader>gC", "<cmd>Telescope git_bcommits<CR>", opts) -- Commits List for current file
+utils.map("n", "<leader>gb", "<cmd>Telescope git_branches<CR>", opts) -- Branches List
+utils.map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", opts) -- Git status
+utils.map("n", "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<CR>", opts) -- Next Hunk
+utils.map("n", "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<CR>", opts) -- Prev Hunk
+utils.map("n", "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<CR>", opts) -- Blame
+utils.map("n", "<leader>gp", "<cmd>lua require 'gitsigns'.preview_hunk()<CR>", opts) -- Preview Hunk
+utils.map("n", "<leader>gr", "<cmd>lua require 'gitsigns'.reset_hunk()<CR>", opts) -- Reset Hunk
+utils.map("n", "<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<CR>", opts) -- Reset Buffer
+utils.map("n", "<leader>gs", "<cmd>lua require 'gitsigns'.stage_hunk()<CR>", opts) -- Stage Hunk
+utils.map("n", "<leader>gu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<CR>", opts) -- Unstage Hunk
 
 -- Help Telescope
-utils.map("n", "<leader>hh", ":Telescope help_tags<CR>", opts) -- help_tags
-utils.map("n", "<leader>ht", ":Telescope builitn<CR>", opts) -- builtins
-utils.map("n", "<leader>hc", ":Telescope commands<CR>", opts) -- commands
-utils.map("n", "<leader>hk", ":Telescope keymaps<CR>", opts) -- keymaps
-utils.map("n", "<leader>ho", ":Telescope vim_options<CR>", opts) -- vim options
-utils.map("n", "<leader>ha", ":Telescope autocommands<CR>", opts) -- autocommands
+utils.map("n", "<leader>hh", "<cmd>Telescope help_tags<CR>", opts) -- help_tags
+utils.map("n", "<leader>ht", "<cmd>Telescope builitn<CR>", opts) -- builtins
+utils.map("n", "<leader>hc", "<cmd>Telescope commands<CR>", opts) -- commands
+utils.map("n", "<leader>hk", "<cmd>Telescope keymaps<CR>", opts) -- keymaps
+utils.map("n", "<leader>ho", "<cmd>Telescope vim_options<CR>", opts) -- vim options
+utils.map("n", "<leader>ha", "<cmd>Telescope autocommands<CR>", opts) -- autocommands
 
--- Code and LSP
-utils.map("n", "<leader>ca", ":Lspsaga code_action<CR>", opts) -- Code actions
-utils.map("n", "<leader>cr", ":Lspsaga rename<CR>", opts) -- Rename function
-utils.map("n", "<leader>cf", ":Lspsaga lsp_finder<CR>", opts) -- Find references
-utils.map("n", "<leader>cs", ":SymbolsOutline<CR>", opts) -- Code tree with symbols
-utils.map("n", "<leader>cd", ":Lspsaga preview_definition<CR>", opts) -- Preview definition
-utils.map("n", "<leader>cF", ":Format<CR>", opts) -- Format buffer
+-- Code and LSP --
+-- Lsp Saga
+utils.map("n", "<leader>cr", "<cmd>Lspsaga rename<CR>", opts) -- Rename function
+utils.map("n", "<leader>cf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- Find references
+utils.map("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- Code actions
+utils.map("n", "<leader>cgn", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+utils.map("n", "<leader>cgp", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+-- Code
+utils.map("n", "<leader>cs", "<cmd>SymbolsOutline<CR>", opts) -- Code tree with symbols
+utils.map("n", "<leader>cd", "<cmd>Lspsaga preview_definition<CR>", opts) -- Preview definition
+utils.map("n", "<leader>cF", "<cmd>Format<CR>", opts) -- Format buffer
 
-utils.map("n", "K", ":lua vim.lsp.buf.hover()<CR>", opts)
-utils.map("n", "<leader>cl", ":lua vim.lsp.diagnostic.set_loclist()<CR>", opts) -- error list
-utils.map("n", "<leader>cgp", "lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-utils.map("n", "<leader>cgn", "lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-utils.map("n", "<leader>cgD", ":lua vim.lsp.buf.definition()<CR>", opts) -- gd: jump to definition
-utils.map("n", "<leader>cgr", ":lua vim.lsp.buf.references()<CR>", opts) -- gr: go to reference
-utils.map("n", "<leader>cgi", ":lua vim.lsp.buf.implementation()<CR>", opts) -- gi: buf implementation
+utils.map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+utils.map("n", "<leader>cl", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts) -- error list
+utils.map("n", "<leader>cgD", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- jump to definition
+utils.map("n", "<leader>cgr", "<cmd>lua vim.lsp.buf.references()<CR>", opts) -- go to reference
+utils.map("n", "<leader>cgi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- buf implementation
 
 -- Runner
-utils.map("n", "<leader>ci", ':lua require("modules.runner").start_repl()<CR>', opts)
+utils.map("n", "<leader>ci", '<cmd>lua require("modules.runner").start_repl()<CR>', opts)
 
 -- Plugins
-utils.map("n", "<leader>pi", ":PackerInstall<CR>", opts)
-utils.map("n", "<leader>ps", ":PackerSync<CR>", opts)
-utils.map("n", "<leader>pc", ":PackerClean<CR>", opts)
-utils.map("n", "<leader>pC", ":PackerCompile<CR>", opts)
-utils.map("n", "<leader>pt", ":PackerStatus<CR>", opts)
-utils.map("n", "<leader>pr", ":lua require('core.functions').reload_config()<CR>", opts)
+utils.map("n", "<leader>pi", "<cmd>PackerInstall<CR>", opts)
+utils.map("n", "<leader>ps", "<cmd>PackerSync<CR>", opts)
+utils.map("n", "<leader>pc", "<cmd>PackerClean<CR>", opts)
+utils.map("n", "<leader>pC", "<cmd>PackerCompile<CR>", opts)
+utils.map("n", "<leader>pt", "<cmd>PackerStatus<CR>", opts)
+utils.map("n", "<leader>pr", "<cmd>lua require('core.functions').reload_config()<CR>", opts)
 
 -- Search
-utils.map("n", "<leader>sg", ":Telescope live_grep<CR>", opts)
-utils.map("n", "<leader>sb", ":Telescope current_buffer_fuzzy_find<CR>", opts)
-utils.map("n", "<leader>ss", ":Telescope lsp_document_symbols<CR>", opts)
-utils.map("n", "<leader>sh", ":Telescope command_history<CR>", opts)
-utils.map("n", "<leader>sm", ":Telescope marks<CR>", opts)
+utils.map("n", "<leader>sg", "<cmd>Telescope live_grep<CR>", opts)
+utils.map("n", "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<CR>", opts)
+utils.map("n", "<leader>ss", "<cmd>Telescope lsp_document_symbols<CR>", opts)
+utils.map("n", "<leader>sh", "<cmd>Telescope command_history<CR>", opts)
+utils.map("n", "<leader>sm", "<cmd>Telescope marks<CR>", opts)
