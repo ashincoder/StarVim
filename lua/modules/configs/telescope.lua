b160da1772b97aef02afe44778c25d2c86d009c8
ashincoder <83629316+ashincoder@users.noreply.github.com>
2021-07-28 09:14:51 +0530

    Added two telescope extensions: media_files and fzf

--- lua/modules/configs/telescope.lua
+++ lua/modules/configs/telescope.lua
@@ -57,5 +57,12 @@ telescope.setup({
 			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 			-- the default case_mode is "smart_case"
 		},
+		media_files = {
+			filetypes = { "png", "webp", "jpg", "jpeg" },
+			find_cmd = "rg", -- find command (defaults to `fd`)
+		},
 	},
 })
+
+require("telescope").load_extension("fzf")
+require("telescope").load_extension("media_files")
