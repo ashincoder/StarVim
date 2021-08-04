87d61a383e08c863425f3bea0e0a4dd405354f88
ashincoder <83629316+ashincoder@users.noreply.github.com>
2021-08-04 19:13:54 +0530

    Added properties to lsp_confgi

--- lua/modules/configs/lsp_config.lua
+++ lua/modules/configs/lsp_config.lua
@@ -18,6 +18,13 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = {
+		"documentation",
+		"detail",
+		"additionalTextEdits",
+	},
+}
 
 -- lspInstall + lspconfig stuff
 
